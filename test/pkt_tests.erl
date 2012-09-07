a597e56189fdfbbccfc538000c9028b06f377e6d
Artem Teslenko <ates@ipv6.dp.ua>
2012-09-07 21:12:07 +0300

    Update tests

--- test/pkt_tests.erl
+++ test/pkt_tests.erl
@@ -6,6 +6,12 @@
 
 sctp_test() ->
     {SCTP, []} = pkt:sctp(?SCTP_PACKET),
+    {SCTP1, []} = pkt:sctp(?SCTP_PACKET_WITH_PADDING),
     Chunk = erlang:hd(SCTP#sctp.chunks),
-    ?assertEqual(length(SCTP#sctp.chunks), 1),
-    ?assertEqual(Chunk#sctp_chunk.payload#sctp_chunk_data.tsn, 2860946939).
+    Chunk1 = erlang:hd(SCTP1#sctp.chunks),
+    ?assertEqual(SCTP#sctp.sport, 7),
+    ?assertEqual(SCTP1#sctp.dport, 32837),
+    ?assertEqual(Chunk#sctp_chunk.type, 1),
+    ?assertEqual(byte_size(Chunk#sctp_chunk.payload), 28),
+    ?assertEqual(length(SCTP1#sctp.chunks), 1),
+    ?assertEqual(Chunk1#sctp_chunk.payload#sctp_chunk_data.tsn, 2860946939).
