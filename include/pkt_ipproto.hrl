4c7c1b25b5f8f178a09044a0cc57272846143da5
Michael Santos <michael.santos@gmail.com>
2013-10-25 11:54:24 -0400

    Add IPv6 extension headers

--- include/pkt_ipproto.hrl
+++ include/pkt_ipproto.hrl
@@ -7,3 +7,12 @@
 -define(IPPROTO_ICMPV6, 58).
 -define(IPPROTO_SCTP, 132).
 -define(IPPROTO_RAW, 255).
+
+% IPV6 extension headers
+-define(IPPROTO_HOPOPTS, 0).    % IPv6 hop-by-hop options
+-define(IPPROTO_ROUTING, 43).   % IPv6 routing header
+-define(IPPROTO_FRAGMENT, 44).  % IPv6 fragmentation header
+-define(IPPROTO_ICMPV6, 58).    % ICMPv6
+-define(IPPROTO_NONE, 59).      % IPv6 no next header
+-define(IPPROTO_DSTOPTS, 60).   % IPv6 destination options
+-define(IPPROTO_MH, 135).       % IPv6 mobility header
