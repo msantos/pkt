805bdcc66140fef51ec3b0ccee5171d05f2dadf7
Artem Teslenko <ates@ipv6.dp.ua>
2014-02-10 13:02:53 +0200

    Remove sctp_ prefix from internal functions

--- src/pkt_sctp.erl
+++ src/pkt_sctp.erl
@@ -36,16 +36,18 @@
 
 -spec codec(binary()) -> {#sctp{}, binary()}.
 codec(<<SPort:16, DPort:16, VTag:32, Sum:32, Payload/binary>>) ->
-    {Chunks, Other} = sctp_decode_chunks(Payload, []),
+    {Chunks, Other} = decode_chunks(Payload, []),
     SCTP = #sctp{
         sport = SPort, dport = DPort, vtag = VTag,
         sum = Sum, chunks = Chunks
     },
     {SCTP, Other}.
 
--spec sctp_decode_chunks(binary(), list()) -> [#sctp_chunk{}].
-sctp_decode_chunks(<<>>, Acc) -> {Acc, <<>>};
-sctp_decode_chunks(<<Type:8, Flags:8, Length:16, Rest/binary>>, Acc) ->
+%% Internal functions
+
+-spec decode_chunks(binary(), list()) -> [#sctp_chunk{}].
+decode_chunks(<<>>, Acc) -> {Acc, <<>>};
+decode_chunks(<<Type:8, Flags:8, Length:16, Rest/binary>>, Acc) ->
     {L, Pad} = case Length rem 4 of
         0 -> % No padding bytes
             {Length - 4, 0};
@@ -53,39 +55,39 @@ sctp_decode_chunks(<<Type:8, Flags:8, Length:16, Rest/binary>>, Acc) ->
             {Length - 4, (4 - N) * 8}
     end,
     <<Payload:L/binary-unit:8, 0:Pad, Tail/binary>> = Rest,
-    sctp_decode_chunks(Tail, [sctp_chunk(Type, Flags, Length, Payload) | Acc]);
+    decode_chunks(Tail, [chunk(Type, Flags, Length, Payload) | Acc]);
 %% Ignore other bytes which are not SCTP chunks (VSS, ...)
-sctp_decode_chunks(Other, Acc) -> {Acc, Other}.
+decode_chunks(Other, Acc) -> {Acc, Other}.
 
--spec sctp_chunk(byte(), byte(), non_neg_integer(), binary()) -> #sctp_chunk{}.
-sctp_chunk(Ctype, Cflags, Clen, Payload) ->
+-spec chunk(byte(), byte(), non_neg_integer(), binary()) -> #sctp_chunk{}.
+chunk(Type, Flags, Len, Payload) ->
 	#sctp_chunk{
-        type = Ctype, flags = Cflags, len = Clen - 4,
-        payload = sctp_chunk_payload(Ctype, Payload)
+        type = Type, flags = Flags, len = Len - 4,
+        payload = chunk_payload(Type, Payload)
     }.
 
--spec sctp_chunk_payload(non_neg_integer(), binary()) -> #sctp_chunk_data{} | binary().
-sctp_chunk_payload(?SCTP_CHUNK_DATA, <<Tsn:32, Sid:16, Ssn:16, Ppi:32, Data/binary>>) ->
+-spec chunk_payload(non_neg_integer(), binary()) -> #sctp_chunk_data{} | binary().
+chunk_payload(?SCTP_CHUNK_DATA, <<Tsn:32, Sid:16, Ssn:16, Ppi:32, Data/binary>>) ->
 	#sctp_chunk_data{tsn = Tsn, sid = Sid, ssn = Ssn, ppi = Ppi, data = Data};
-sctp_chunk_payload(?SCTP_CHUNK_INIT, <<Itag:32, Arwnd:32, OutStreams:16, InStreams:16, Tsn:32, Rest/binary>>) ->
+chunk_payload(?SCTP_CHUNK_INIT, <<Itag:32, Arwnd:32, OutStreams:16, InStreams:16, Tsn:32, Rest/binary>>) ->
     #sctp_chunk_init{
         itag = Itag,
         a_rwnd = Arwnd,
         outbound_streams = OutStreams,
         inbound_streams = InStreams,
         tsn = Tsn,
-        params = sctp_init_params(Rest, [])
+        params = init_params(Rest, [])
     };
-sctp_chunk_payload(?SCTP_CHUNK_INIT_ACK, <<Itag:32, Arwnd:32, OutStreams:16, InStreams:16, Tsn:32, Rest/binary>>) ->
+chunk_payload(?SCTP_CHUNK_INIT_ACK, <<Itag:32, Arwnd:32, OutStreams:16, InStreams:16, Tsn:32, Rest/binary>>) ->
     #sctp_chunk_init_ack{
         itag = Itag,
         a_rwnd = Arwnd,
         outbound_streams = OutStreams,
         inbound_streams = InStreams,
         tsn = Tsn,
-        params = sctp_init_params(Rest, [])
+        params = init_params(Rest, [])
     };
-sctp_chunk_payload(?SCTP_CHUNK_SACK, <<TSN_ACK:32, Arwnd:32, GapsN:16, DuplicateTSN:16, Rest/binary>>) ->
+chunk_payload(?SCTP_CHUNK_SACK, <<TSN_ACK:32, Arwnd:32, GapsN:16, DuplicateTSN:16, Rest/binary>>) ->
     GapsLength = GapsN * 4, %% Gap Ack start (16), Gap Ack end (16)
     <<Gaps:GapsLength/binary-unit:8, TSNs/binary>> = Rest,
     #sctp_chunk_sack{
@@ -96,54 +98,54 @@ sctp_chunk_payload(?SCTP_CHUNK_SACK, <<TSN_ACK:32, Arwnd:32, GapsN:16, Duplicate
         gap_ack_blocks = [{Start, End} || <<Start:16, End:16>> <= Gaps],
         duplicate_tsns = [T || <<T:32>> <= TSNs]
     };
-sctp_chunk_payload(?SCTP_CHUNK_COOKIE_ECHO, Cookie) ->
+chunk_payload(?SCTP_CHUNK_COOKIE_ECHO, Cookie) ->
     #sctp_chunk_cookie_echo{cookie = Cookie};
-sctp_chunk_payload(?SCTP_CHUNK_COOKIE_ACK, <<>>) ->
+chunk_payload(?SCTP_CHUNK_COOKIE_ACK, <<>>) ->
     #sctp_chunk_cookie_ack{};
-sctp_chunk_payload(?SCTP_CHUNK_HEARTBEAT, <<Type:16, _Length:16, Info/binary>>) ->
+chunk_payload(?SCTP_CHUNK_HEARTBEAT, <<Type:16, _Length:16, Info/binary>>) ->
     #sctp_chunk_heartbeat{type = Type, info = Info};
-sctp_chunk_payload(?SCTP_CHUNK_HEARTBEAT_ACK, <<Type:16, _Length:16, Info/binary>>) ->
+chunk_payload(?SCTP_CHUNK_HEARTBEAT_ACK, <<Type:16, _Length:16, Info/binary>>) ->
     #sctp_chunk_heartbeat_ack{type = Type, info = Info};
-sctp_chunk_payload(?SCTP_CHUNK_SHUTDOWN, <<TSN_ACK:32>>) ->
+chunk_payload(?SCTP_CHUNK_SHUTDOWN, <<TSN_ACK:32>>) ->
     #sctp_chunk_shutdown{tsn_ack = TSN_ACK};
-sctp_chunk_payload(?SCTP_CHUNK_SHUTDOWN_ACK, <<>>) ->
+chunk_payload(?SCTP_CHUNK_SHUTDOWN_ACK, <<>>) ->
     #sctp_chunk_shutdown_ack{};
-sctp_chunk_payload(?SCTP_CHUNK_SHUTDOWN_COMPLETE, <<>>) ->
+chunk_payload(?SCTP_CHUNK_SHUTDOWN_COMPLETE, <<>>) ->
     #sctp_chunk_shutdown_complete{};
-sctp_chunk_payload(?SCTP_CHUNK_ABORT, Errors) ->
-    #sctp_chunk_abort{error_causes = sctp_error_causes(Errors, [])};
-sctp_chunk_payload(_, Data) ->
+chunk_payload(?SCTP_CHUNK_ABORT, Errors) ->
+    #sctp_chunk_abort{error_causes = error_causes(Errors, [])};
+chunk_payload(_, Data) ->
 	Data.
 
-sctp_init_params(<<>>, Acc) -> Acc;
+init_params(<<>>, Acc) -> Acc;
 
 %% IPv4 Address Parameter
-sctp_init_params(<<5:16, 8:16, A:8, B:8, C:8, D:8, Rest/binary>>, Acc) ->
-    sctp_init_params(Rest, [{ipv4, {A, B, C, D}} | Acc]);
+init_params(<<5:16, 8:16, A:8, B:8, C:8, D:8, Rest/binary>>, Acc) ->
+    init_params(Rest, [{ipv4, {A, B, C, D}} | Acc]);
 %% IPv6 Address Parameter
-sctp_init_params(<<6:16, 20:16, Value:16/binary-unit:8, Rest/binary>>, Acc) ->
+init_params(<<6:16, 20:16, Value:16/binary-unit:8, Rest/binary>>, Acc) ->
     IP = list_to_tuple([N || <<N:16>> <= Value]),
-    sctp_init_params(Rest, [{ipv6, IP} | Acc]);
+    init_params(Rest, [{ipv6, IP} | Acc]);
 %% State cookie
-sctp_init_params(<<7:16, Length:16, Rest/binary>>, Acc) ->
+init_params(<<7:16, Length:16, Rest/binary>>, Acc) ->
     L = Length - 4,
     <<Cookie:L/binary-unit:8, Tail/binary>> = Rest,
-    sctp_init_params(Tail, [{state_cookie, Cookie} | Acc]);
+    init_params(Tail, [{state_cookie, Cookie} | Acc]);
 %% Unrecognized Parameter
-sctp_init_params(<<8:16, Length:16, Rest/binary>>, Acc) ->
+init_params(<<8:16, Length:16, Rest/binary>>, Acc) ->
     L = Length - 4,
     <<Parameter:L/binary-unit:8, Tail/binary>> = Rest,
-    sctp_init_params(Tail, [{unrecognized, Parameter} | Acc]);
+    init_params(Tail, [{unrecognized, Parameter} | Acc]);
 %% Cookie Preservative
-sctp_init_params(<<9:16, 8:16, Value:32, Rest/binary>>, Acc) ->
-    sctp_init_params(Rest, [{cookie, Value} | Acc]);
+init_params(<<9:16, 8:16, Value:32, Rest/binary>>, Acc) ->
+    init_params(Rest, [{cookie, Value} | Acc]);
 %% Host Name Address
-sctp_init_params(<<11:16, Length:16, Rest/binary>>, Acc) ->
+init_params(<<11:16, Length:16, Rest/binary>>, Acc) ->
     L = Length - 4,
     <<Hostname:L/binary-unit:8, Tail/binary>> = Rest,
-    sctp_init_params(Tail, [{hostname, Hostname} | Acc]);
+    init_params(Tail, [{hostname, Hostname} | Acc]);
 %% Supported Address Types
-sctp_init_params(<<12:16, Length:16, Rest/binary>>, Acc) ->
+init_params(<<12:16, Length:16, Rest/binary>>, Acc) ->
     AddressType =
         fun(5) -> ipv4;
            (6) -> ipv6;
@@ -152,25 +154,25 @@ sctp_init_params(<<12:16, Length:16, Rest/binary>>, Acc) ->
     case Length rem 4 of
         0 ->
             <<Value:16, Tail/binary>> = Rest,
-            sctp_init_params(Tail, [{address_type, AddressType(Value)} | Acc]);
+            init_params(Tail, [{address_type, AddressType(Value)} | Acc]);
         N ->
             <<Value:16, _Padding:N/binary-unit:8, Tail/binary>> = Rest,
-            sctp_init_params(Tail, [{address_type, AddressType(Value)} | Acc])
+            init_params(Tail, [{address_type, AddressType(Value)} | Acc])
     end;
 %% Ignore ECN and Forward TSN parameters
-sctp_init_params(_, Acc) -> Acc.
+init_params(_, Acc) -> Acc.
 
-sctp_error_causes(<<>>, Acc) ->
+error_causes(<<>>, Acc) ->
     Acc;
-sctp_error_causes(<<Code:16, Length:16, Rest/binary>>, Acc) ->
+error_causes(<<Code:16, Length:16, Rest/binary>>, Acc) ->
     L = Length - 4,
     <<Opts:L/binary-unit:8, Tail/binary>> = Rest,
-    sctp_error_causes(Tail, [sctp_error(Code, L, Opts) | Acc]).
+    error_causes(Tail, [sctp_error(Code, L, Opts) | Acc]).
 
 sctp_error(1, _Length, <<Ident:16, _Reserved:8>>) ->
     #sctp_error_cause{
         code = 1,
-        descr = sctp_format_error(1),
+        descr = format_error(1),
         opts = [
             {stream_identifier, Ident}
         ]
@@ -179,7 +181,7 @@ sctp_error(12, Length, Opts) ->
     <<Reason:Length/binary-unit:8>> = Opts,
     #sctp_error_cause{
         code = 12,
-        descr = sctp_format_error(12),
+        descr = format_error(12),
         opts = [
             {abort_reason, Reason}
         ]
@@ -188,35 +190,37 @@ sctp_error(12, Length, Opts) ->
 sctp_error(Code, _Length, Opts) ->
     #sctp_error_cause{
         code = Code,
-        descr = sctp_format_error(Code),
+        descr = format_error(Code),
         opts = [
             {data = Opts}
         ]
     }.
 
-sctp_format_error(1) ->
+%% TODO: Replace this by using gen_sctp:error_string/1
+%% Full all of errors were added since R16B03
+format_error(1) ->
     "Invalid Stream Identifier";
-sctp_format_error(2) ->
+format_error(2) ->
     "Missing Mandatory Parameter";
-sctp_format_error(3) ->
+format_error(3) ->
     "Stale Cookie Error";
-sctp_format_error(4) ->
+format_error(4) ->
     "Out of Resource";
-sctp_format_error(5) ->
+format_error(5) ->
     "Unresolvable Address";
-sctp_format_error(6) ->
+format_error(6) ->
     "Unrecognized Chunk Type";
-sctp_format_error(7) ->
+format_error(7) ->
     "Invalid Mandatory Parameter";
-sctp_format_error(8) ->
+format_error(8) ->
     "Unrecognized Parameters";
-sctp_format_error(9) ->
+format_error(9) ->
     "No User Data";
-sctp_format_error(10) ->
+format_error(10) ->
     "Cookie Received While Shutting Down";
-sctp_format_error(11) ->
+format_error(11) ->
     "Restart of an Association with New Addresses";
-sctp_format_error(12) ->
+format_error(12) ->
     "User Initiated Abort";
-sctp_format_error(13) ->
+format_error(13) ->
     "Protocol Violation".
