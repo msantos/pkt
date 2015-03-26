895e0d7e4637d41c9e0fbb9e2edb75bdc73b358e
Michael Santos <michael.santos@gmail.com>
2015-03-26 08:16:34 -0400

    pkt_tcp: add type specs to record

--- include/pkt_tcp.hrl
+++ include/pkt_tcp.hrl
@@ -1,9 +1,9 @@
 -record(tcp, {
-        sport = 0, dport = 0,
-        seqno = 0,
-        ackno = 0,
-        off = 5, cwr = 0, ece = 0, urg = 0, ack = 0,
-        psh = 0, rst = 0, syn = 0, fin = 0, win = 0,
-        sum = 0, urp = 0,
-        opt = <<>>
+        sport = 0 :: pkt:in_port_t(), dport = 0 :: pkt:in_port_t(),
+        seqno = 0 :: pkt:uint32_t(),
+        ackno = 0 :: pkt:uint32_t(),
+        off = 5 :: pkt:nibble(), cwr = 0 :: 0 | 1, ece = 0 :: 0 | 1, urg = 0 :: 0 | 1, ack = 0 :: 0 | 1,
+        psh = 0 :: 0 | 1, rst = 0 :: 0 | 1, syn = 0 :: 0 | 1, fin = 0 :: 0 | 1, win = 0 :: pkt:uint16_t(),
+        sum = 0 :: pkt:uint16_t(), urp = 0 :: pkt:uint16_t(),
+        opt = <<>> :: binary()
     }).
