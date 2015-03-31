5d6a156ad8335bb39edd8089d4cf26815fda3b4c
Michael Santos <michael.santos@gmail.com>
2015-03-31 07:32:45 -0400

    801_q: add spec for priority field

--- include/pkt_802_1q.hrl
+++ include/pkt_802_1q.hrl
@@ -1,6 +1,6 @@
 -record('802.1q', {
     tpid :: 1.. 65535, %% Tag Protocol Identifier
-    prio, %% Priority
+    prio :: 0..2#111, %% Priority
     cfi = 0 :: 0 | 1, %% Canonical Format Indicator
     vid :: 0..4095 %% VLAN Identifier
 }).
