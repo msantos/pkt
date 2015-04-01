65b7c1f41905ebd9dceec91efe051caecf96e719
Michael Santos <michael.santos@gmail.com>
2015-04-01 09:22:16 -0400

    gre: add specs to record

--- include/pkt_gre.hrl
+++ include/pkt_gre.hrl
@@ -1,6 +1,6 @@
 %% RFC 2784 - Generic Routing Encapsulation (GRE)
 -record(gre, {
-        c = 0, res0 = 0, ver = 0,
-        type = ?ETH_P_IP,
-        chksum = <<>>, res1 = <<>>
+        c = 0 :: pkt:bit(), res0 = 0 :: 0..2#111111111111, ver = 0 :: 0..2#111,
+        type = ?ETH_P_IP :: pkt:uint16_t(),
+        chksum = 0 :: pkt:uint16_t(), res1 = 0 :: pkt:uint16_t()
     }).
