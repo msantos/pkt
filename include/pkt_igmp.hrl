283a58c94c01aedb0cde0e1033b3fc4af948acd5
Michael Santos <michael.santos@gmail.com>
2015-04-02 06:23:55 -0400

    igmp: add types to record

--- include/pkt_igmp.hrl
+++ include/pkt_igmp.hrl
@@ -1,14 +1,13 @@
--record(igmp, {
-        type = 0,
-        code = 0,
-        csum = 0,
-        group
-    }).
-
-
 -record(igmp_group, {
   type = 0,
   addr,
   sources = [],
   aux = <<>>
   }).
+
+-record(igmp, {
+        type = 0 :: byte(),
+        code = 0 :: byte(),
+        csum = 0 :: pkt:uint16_t(),
+        group :: pkt:in_addr() | [#igmp_group{}]
+    }).
