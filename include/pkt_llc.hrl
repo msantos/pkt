c225a0040d6eeb5ddc4429d6a3ca7aa0fdcd1756
Michael Santos <michael.santos@gmail.com>
2015-04-09 09:33:01 -0400

    llc: add types to record

--- include/pkt_llc.hrl
+++ include/pkt_llc.hrl
@@ -1,8 +1,8 @@
 -record(llc, {
-    dsap = 16#AA, % Destination Service Access Point
-    ssap = 16#AA, % Source Service Access Point
-    control = 16#03,
+    dsap = 16#AA :: pkt:uint8_t(), % Destination Service Access Point
+    ssap = 16#AA :: pkt:uint8_t(), % Source Service Access Point
+    control = 16#03 :: pkt:uint8_t(),
     %% SNAP (Sub-Network Access Protocol) fields
-    vendor = <<0, 0, 0>>,
-    pid = 0 % Ethertype for the frame
+    vendor = <<0, 0, 0>> :: <<_:24>>,
+    pid = 0 :: pkt:uint16_t() % Ethertype for the frame
 }).
