0cbeee57837cb236b95d47be55b2f5d3fdbe7030
Michael Santos <michael.santos@gmail.com>
2015-04-15 09:50:19 -0400

    udp: add types to record

--- include/pkt_udp.hrl
+++ include/pkt_udp.hrl
@@ -1,3 +1,3 @@
 -record(udp, {
-        sport = 0, dport = 0, ulen = 8, sum = 0
+        sport = 0 :: pkt:in_port_t(), dport = 0 :: pkt:in_port_t(), ulen = 8 :: pkt:uint16_t(), sum = 0 :: pkt:uint16_t()
     }).
