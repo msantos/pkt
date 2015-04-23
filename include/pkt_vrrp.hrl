c8660309b16930684213e513431b308b55e49527
Michael Santos <michael.santos@gmail.com>
2015-04-23 10:07:42 -0400

    vrrp: add types to records

--- include/pkt_vrrp.hrl
+++ include/pkt_vrrp.hrl
@@ -1,10 +1,10 @@
 -record(vrrp, {
-    version = 2,
-    type = 1,
+    version = 2 :: pkt:bit4(),
+    type = 1 :: pkt:bit4(),
     vrid = 1 :: 1..255,
     priority = 100 :: 1..255,
-    auth_type = 0,
-    adver_int = 1,
-    sum = 0,
-    ip_addresses = []
+    auth_type = 0 :: pkt:uint8_t(),
+    adver_int = 1 :: pkt:uint8_t(),
+    sum = 0 :: pkt:uint16_t(),
+    ip_addresses = [] :: [pkt:in_addr()]
 }).
