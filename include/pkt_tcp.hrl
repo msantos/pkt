4f0cb2ca796b2647127d81b98f9af7fc20b0c5c0
Artem Teslenko <ates@ipv6.dp.ua>
2016-03-17 11:32:48 +0200

    Fix specs for #tcp{}

--- include/pkt_tcp.hrl
+++ include/pkt_tcp.hrl
@@ -1,9 +1,19 @@
 -record(tcp, {
-        sport = 0 :: pkt:in_port_t(), dport = 0 :: pkt:in_port_t(),
-        seqno = 0 :: pkt:uint32_t(),
-        ackno = 0 :: pkt:uint32_t(),
-        off = 5 :: pkt:bit4(), cwr = 0 :: 0 | 1, ece = 0 :: 0 | 1, urg = 0 :: 0 | 1, ack = 0 :: 0 | 1,
-        psh = 0 :: 0 | 1, rst = 0 :: 0 | 1, syn = 0 :: 0 | 1, fin = 0 :: 0 | 1, win = 0 :: pkt:uint16_t(),
-        sum = 0 :: pkt:uint16_t(), urp = 0 :: pkt:uint16_t(),
-        opt = <<>> :: binary()
-    }).
+    sport = 0 :: pkt:in_port_t(),
+    dport = 0 :: pkt:in_port_t(),
+    seqno = 0 :: pkt:uint32_t(),
+    ackno = 0 :: pkt:uint32_t(),
+    off = 5 :: pkt:bit4(),
+    cwr = 0 :: pkt:bit(),
+    ece = 0 :: pkt:bit(),
+    urg = 0 :: pkt:bit(),
+    ack = 0 :: pkt:bit(),
+    psh = 0 :: pkt:bit(),
+    rst = 0 :: pkt:bit(),
+    syn = 0 :: pkt:bit(),
+    fin = 0 :: pkt:bit(),
+    win = 0 :: pkt:uint16_t(),
+    sum = 0 :: pkt:uint16_t(),
+    urp = 0 :: pkt:uint16_t(),
+    opt = <<>> :: binary()
+}).
