5baf449b7dc46ac7d92289d1c7a209bd7685368b
Stephen Han <stephen.han@hmatix.com>
2017-12-28 17:56:37 -0800

    Update comments for pkt_802_1x.hrl

--- include/pkt_802_1x.hrl
+++ include/pkt_802_1x.hrl
@@ -1,8 +1,8 @@
--define(EAP_PACKET, 0).           % Contains encapsulate EAP frame
--define(EAPOL_START, 1).          % A supplicant issued frame  
--define(EAPOL_LOGOFF, 2).         % Logoff to switch port to unauthorized state
--define(EAPOL_KEY, 3).            % Exchange cryptographic keying information
--define(EAPOL_ENCAPSULATED_ASF_ALERT, 4). % Alerting unauthorized state 
+-define(EAP_PACKET, 0).           % EAPOL-PACKET 
+-define(EAPOL_START, 1).          % EAPOL-START  
+-define(EAPOL_LOGOFF, 2).         % EAPOL-LOGOFF
+-define(EAPOL_KEY, 3).            % EAPOL-KEY
+-define(EAPOL_ENCAPSULATED_ASF_ALERT, 4). % EAPOL-ENCAPSULATED-ASF-ALERT
 
 -record('802.1x', {
 	  ver = 2 :: pkt:uint8_t(),
